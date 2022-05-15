491344491ebfd01c6255caa9de84a233bfc0b8d8
Maksim Dzmitryew̆ <maksimdzmitryew@gmail.com>
2022-05-16 00:28:53 +0200

    Refactoring, split into classes, set types, unify methods

--- CacheManager.php
+++ CacheManager.php
@@ -1,118 +1,71 @@
 <?php
+
+declare(strict_types = 1);
+
 /**
  * Created by PhpStorm.
  * User: isnain
  * Date: 09.08.21
  * Time: 10:14
+ *
+ * PHP v.7.2+
  */
+class CacheManager implements CacheManagerInterface
+{
+    private CacheManagerInterface $cacheConnector;
 
-interface CacheManagerInterface{
-    public function connect(string $host,string $port);
-    public function set(string $key, string $value, string $is_compressed = null, string $ttl = null);
-    public function get(string $key): string;
-    public function lpush(string $key, string $value);        
-}
-
-class RedisCacheConnector {
-    private $reids;
-
-    public function __construct() {
-        $this->reids = new \Redis();
+    public function __construct (string $cacheConnector)
+    {
+        $this->cacheConnector = CacheConnectionFactory::connect($cacheConnector);
     }
 
-    public function connect(string $host,string $port) {
-        $this->reids->connect($host,$port);
-    }
-
-    public function lpush(string $key, string $value) {
-        $this->reids->lPush($key,$value); 
-    }
-
-}
-
-class MemCacheConnector {
-    private $memcache;
-
-    public function __construct() {
-        $this->memcache = new \Memcache();
-    }
-
-    public function connect(string $host,string $port): void {
-        $this->memcache->connect($host,$port);
-    }
-
-    public function lpush(string $key, string $value) {
-        throw new \Exception("method not supported");
-    }
-}
-
-class CacheConnectionFactory {
-    private const REDIS = 'redis';
-    private const MEMCACHED = 'memcached';
-
-    public static function connect(string $driverName = "redis") {
-        if (strtolower($driverName) === self::REDIS) {
-            return new RedisConnector();
-        } else if (strtolower($driverName) === self::MEMCACHED) {
-            return new MemCacheConnector();
-        } else {
-            throw new Exception('Invalid Driver');
-        }
-
-        return null;
-    }
-}
-
-class CacheManager implements CacheManagerInterface{
-
-    private $cacheConnector;
-
-    public function __construct($cacheConnector) {
-        $this->cacheConnector = $cacheConnector;
-
-        // or you can use connection factory
-        $connection = 'redis'; // this variable should be passed to construct
-        $this->cacheConnector = CacheConnectionFactory::connect($connection);
-    }
-
-    public function connect(string $host,string $port): self   {
-        $this->cacheConnector->connect($host,$port);
+    public function connect (string $host, string $port) : CacheManagerInterface
+    {
+        $this->cacheConnector->connect($host, $port);
 
         return $this;
-    }    
+    }
 
-    public function set(string $key, string $value, string $is_compressed = null, string $ttl = null): self  {
+    public function set (string $key, string $value, string $is_compressed = null, string $ttl = null) : CacheManagerInterface
+    {
         $this->cacheConnector->set($key, $value, $is_compressed, $ttl);
 
         return $this;
     }
 
-    public function get(string $key): string {
+    public function get (string $key) : string
+    {
         return $this->cacheConnector->get($key);
     }
 
-    public function lpush(string $key, string $value): self {
+    public function lpush (string $key, string $value) : CacheManagerInterface
+    {
         $this->cacheConnector->lpush($key, $value);
+
         return $this;
     }
 }
 
-// if you want to use connection factory then following line
-$redisCache2 = new CacheManager('redis');
+try
+{
+    // if you want to use connection factory then following line
+    $redisCache = new CacheManager(CacheConnectionFactory::REDIS);
 
-$redisCache = new CacheManager(new RedisCacheConnector());
-$redisCache->connect('somehost','121')
-    ->set('one','1')
-    ->lpush('two','1')
-    ->lpush('two','2');
+    $redisCache->connect('somehost', '121')
+        ->set('one', '1')
+        ->lpush('two', '1')
+        ->lpush('two', '2');
 
-echo $redisCache->get('one');
+    echo $redisCache->get('one');
 
-$memCache = new CacheManager(new MemCacheConnector());
-$memCache->connect('somehost','121')
-    ->set('one','1')
-    ->lpush('two','2'); // generates exception
+    $memCache = new CacheManager(CacheConnectionFactory::MEMCACHED);
 
-echo $memCache->get('one');
-?>
-                            
+    $memCache->connect('somehost', '121')
+        ->set('one', '1')
+        ->lpush('two', '2'); // generates exception
+    echo $memCache->get('one');
+}
+catch (Exception $e)
+{
+    //TODO write error message to log $e->getMessage()
+}
